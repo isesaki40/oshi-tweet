f83f24ec7ad385f11e72ba27f2c868a9ad152b2f
NAKARI <nkrng118@gmail.com>
2022-07-09 19:21:44 +0900

    日付追加、フォーマット整理

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,28 +10,44 @@ export const Form = () => {
   } = useForm();
   const onSubmit = (data) => console.log("onSubmit:", data);
   console.log("watch:", watch("example")); // watchは引数に渡した名前の入力値を監視する
+  /*
+  var date = new Date();
+  var yyyy = date.getFullYear();
+  var mm = ("0"+(date.getMonth()+1)).slice(-2);
+  var dd = ("0"+date.getDate()).slice(-2);
+  document.getElementById("today").value=yyyy+'-'+mm+'-'+dd;
+  ここがあると動いたり動かなかったりする
+  */
   return (
     /* handleSubmitはフォームの入力を確かめたうえで、引数に渡した関数(onSubmit)を呼び出す */
     <form onSubmit={handleSubmit(onSubmit)}>
       {/* register関数の呼び出しにより、フォーム入力の要素を引数の名前で登録する */}
-      <label>
-        OshiName
-        <input defaultValue="test" {...register("example")} />
-      </label>
-      <br></br>
-      <label>
-        FirstDate
-        <input type="date" {...register("firstDate", { valueAsDate: true })} />
-        {/* 始めの日付 */}
-      </label>
-      <br></br>
-      <label>
-        LastDate
-        <input type="date" {...register("lastDate", { valueAsDate: true })} />
-        {/* 終わりの日付 */}
-      </label>
-      <br></br>
-      <input type="submit" />
+      <div style={{'height':'15em','background-color': '#00FF00', 'font-family': 'ＭＳ ゴシック'}} align='center'>
+        <br></br>
+        推しの名前　
+        <br></br>
+        <input defaultValue=""　placeholder="木村　拓也" {...register("oshiName")} />
+        <br></br>
+        <br></br>
+        検索期間
+        <br></br>
+        <table> 
+          <tr>
+            <th align='left'>from:</th>
+            <th><input type="date" {...register("firstDate", { valueAsDate: true })} /></th>
+            {/* 始めの日付 */}
+          </tr>
+          <tr></tr>
+          <tr>
+            <th align='left'>to:</th>
+            <th><input type="date"　id='today' {...register("lastDate", { valueAsDate: true })} /></th>
+            {/* 終わりの日付 */}
+          </tr>
+          <br></br>
+          <br></br>
+        </table>
+        <input type="submit" />
+      </div>
     </form>
   );
 };
