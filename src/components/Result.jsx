4c19156ef17fbeaa9753081bb22475e74e8f9df8
isosea <isokazu.psy@gmail.com>
2022-07-09 19:33:25 +0900

    ✨ axios導入

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,17 +1,39 @@
-import { getQueriesForElement } from "@testing-library/react";
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
+import { getTest } from "../apis/getTest";
+import { getSearch } from "../apis/getSearch";
+
+const test = async () => {
+  try {
+    const res = await getTest();
+    console.log(res);
+  } catch (e) {
+    console.error("Error: ", e);
+  }
+};
+
+const search = async () => {
+  try {
+    const res = await getSearch("(apple OR iphone) ipad");
+    console.log(res);
+  } catch (e) {
+    console.error("Error: ", e);
+  }
+};
+
+test();
+search();
 
 export const Result = () => {
   let oshi_count = 10;
-  return(
-    <div style={{'height':'60em','background-color': '#00FF00'}}>
+  return (
+    <div style={{ height: "60em", "background-color": "#00FF00" }}>
       <h1>Result</h1>
       <Title>あなたの推しツイートした回数は</Title>
-        <Count>{oshi_count}回</Count>
+      <Count>{oshi_count}回</Count>
     </div>
-  ) 
-}
+  );
+};
 
 const Count = styled.h1`
   font-size:5em;
@@ -22,5 +44,5 @@ const Count = styled.h1`
 const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
-  padding-top:0.3em;
+  padding-top: 0.3em;
 `;
---
+++ src/urls/index.js
@@ -0,0 +1,2 @@
+export const test = "/2/tweets/20";
+export const search = "/2/tweets/search/recent";
