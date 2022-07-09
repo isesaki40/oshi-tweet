03b1502ee66d426423de9676c5b168855e9b75c1
mahiro <c1102864@planet.kanazawa-it.ac.jp>
2022-07-09 19:02:25 +0900

    a

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,26 +2,49 @@ import { getQueriesForElement } from "@testing-library/react";
 import React from "react";
 import styled from 'styled-components';
 
-export const Result = () => {
-  let oshi_count = 10;
+/*const redirectToTwitter = () => {*/
+  /*window.location.href = "https://twitter.com/ " + "{props.arg1}";*/
+
+
+export const Result = (props) => {
   return(
-    <div style={{'height':'60em','background-color': '#00FF00'}}>
+    <div style={{'height':'45em','background-color': '#00FF00'}}>
       <h1>Result</h1>
       <Title>あなたの推しツイートした回数は</Title>
-        <Count>{oshi_count}回</Count>
+      <Count>{props.count}回</Count>
+      <img src={props.img}></img>
+      <Name>{props.name}さん<br/>
+      @{props.id}</Name>
+      <User>by{props.user}さん @{props.userid}</User>
     </div>
-    /*aaaaaaaaaa*/
-  ) 
+  )
 }
 
 const Count = styled.h1`
-  font-size:5em;
-  text-align:center;
-  padding:-top:0.5em;
+  font-size: 5em;
+  text-align: center;
+  padding:-top: 0.5em;
 `;
 
 const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
-  padding-top:0.3em;
+  padding-top: 0.3em;
 `;
+
+const Name = styled.h2`
+  text-align:center;
+  font-size: 2em;
+  padding-top:0.9em;
+`;
+
+
+const User = styled.h2`
+  letter-spacing:0.1em;
+  float:right;
+  font-size: 1.5em;
+  padding:2em 0em 0em 0em;
+
+`
+
+export default Result;
