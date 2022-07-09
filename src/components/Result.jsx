a4c5e7065cdb9e22414962b2c8b39ffdda7cdb81
mahiro <c1102864@planet.kanazawa-it.ac.jp>
2022-07-09 14:46:09 +0900

    result

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,5 +1,26 @@
+import { getQueriesForElement } from "@testing-library/react";
 import React from "react";
+import styled from 'styled-components';
 
 export const Result = () => {
-  return <div>Result</div>;
-};
+  let oshi_count = 10;
+  return(
+    <div style={{'height':'60em','background-color': '#00FF00'}}>
+      <h1>Result</h1>
+      <Title>あなたの推しツイートした回数は</Title>
+        <Count>{oshi_count}回</Count>
+    </div>
+  ) 
+}
+
+const Count = styled.h1`
+  font-size:5em;
+  text-align:center;
+  padding:-top:0.5em;
+`;
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  padding-top:0.3em;
+`;
